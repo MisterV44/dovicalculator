618a1368c70383a20008ac36d9cfa174e7aeb15a
MisterV44 <78601279+MisterV44@users.noreply.github.com>
2023-01-23 16:15:19 +0000

    evolution

--- app/src/main/java/com/example/dovicaluculator/MainActivity.kt
+++ app/src/main/java/com/example/dovicaluculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.dovicaluculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun numberAction(view: View) {}
+    fun operationAction(view: View) {}
 }
